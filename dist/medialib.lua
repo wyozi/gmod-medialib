80ffe2b656b09d4dc176dc7e914261a1dd900570
wyozi <joonasmertanen@gmail.com>
2015-03-03 22:21:37 +0200

    Update build

--- dist/medialib.lua
+++ dist/medialib.lua
@@ -170,27 +170,48 @@ end
 -- Module service_html
 do
 	local oop = medialib.load("oop")
+	local panel_width, panel_height = 1280, 720
 	local HTMLMedia = oop.class("HTMLMedia", "Media")
 	function HTMLMedia:initialize()
 		self.panel = vgui.Create("DHTML")
 		local pnl = self.panel
 		pnl:SetPos(0, 0)
+		pnl:SetSize(panel_width, panel_height)
 		pnl:SetPaintedManually(true)
 		pnl:SetVisible(false)
+		pnl:AddFunction("medialiblua", "Event", function(id, jsonstr)
+			local json = util.JSONToTable(jsonstr)
+			print("Received event ", id, ": ", table.ToString(json))
+		end)
 	end
 	function HTMLMedia:stop()
 		self.panel:Remove()
 	end
 	function HTMLMedia:draw(x, y, w, h)
-		self.panel:SetSize(w, h)
 		self.panel:UpdateHTMLTexture()
-		local mat = self:GetHTMLMaterial()
+		local mat = self.panel:GetHTMLMaterial()
 		surface.SetMaterial(mat)
 		surface.SetDrawColor(255, 255, 255)
-		surface.DrawTexturedRect(x, y, w, h)
+		local w_frac, h_frac = panel_width / mat:Width(), panel_height / mat:Height()
+		surface.DrawTexturedRectUV(0, 0, w or panel_width, h or panel_height, 0, 0, w_frac, h_frac)
 	end
 	function HTMLMedia:openUrl(url)
 		self.panel:OpenURL(url)
+	end
+	function HTMLMedia:runJS(js)
+		self.panel:QueueJavascript(js)
+	end
+	function HTMLMedia:setVolume(vol)
+		self:runJS(string.format("medialibjs.setVolume(%f)", vol))
+	end
+	function HTMLMedia:play()
+		self:runJS("medialibjs.play()")
+	end
+	function HTMLMedia:pause()
+		self:runJS("medialibjs.pause()")
+	end
+	function HTMLMedia:stop()
+		self.panel:Remove()
 	end
 	local HTMLService = oop.class("HTMLService", "Service")
 end
@@ -250,12 +271,14 @@ function YoutubeService:load(url)\
 \9local media = oop.class(\"HTMLMedia\")()\
 \
 \9local urlData = self:parseUrl(url)\
-\9local playerUrl = \"http://wyozi.github.io/wmc/players/youtube.html?id=\" .. urlData.id\
+\9local playerUrl = \"http://wyozi.github.io/gmod-medialib/youtube.html?id=\" .. urlData.id\
 \
 \9media:openUrl(playerUrl)\
 \
 \9return media\
-end"
+end\
+\
+medialib.load(\"media\").RegisterService(\"youtube\", YoutubeService)"
 -- Module servicebase
 do
 	local oop = medialib.load("oop")

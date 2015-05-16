284fc403e8bd6e9c4e5d3fc3aa4e1b130b7f80b0
wyozi <joonasmertanen@gmail.com>
2015-05-16 15:38:46 +0300

    Add MP4 service

---
+++ lua/medialib/services/mp4.lua
@@ -0,0 +1,34 @@
+local oop = medialib.load("oop")
+
+local Mp4Service = oop.class("Mp4Service", "HTMLService")
+
+local all_patterns = {"^https?://.*%.mp4"}
+
+function Mp4Service:parseUrl(url)
+	for _,pattern in pairs(all_patterns) do
+		local id = string.match(url, pattern)
+		if id then
+			return {id = id}
+		end
+	end
+end
+
+function Mp4Service:isValidUrl(url)
+	return self:parseUrl(url) ~= nil
+end
+
+local player_url = "http://wyozi.github.io/gmod-medialib/mp4.html?id=%s"
+function Mp4Service:resolveUrl(url, callback)
+	local urlData = self:parseUrl(url)
+	local playerUrl = string.format(player_url, urlData.id)
+
+	callback(playerUrl, {start = urlData.start})
+end
+
+function Mp4Service:query(url, callback)
+	callback(nil, {
+		title = url:match("([^/]+)$")
+	})
+end
+
+medialib.load("media").registerService("mp4", Mp4Service)
